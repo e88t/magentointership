3e6ec32072e3dd931c0c0de2acfbf9cfd7216aa4
Eugen <eugentokariuk@gmail.com>
2021-03-12 16:03:03 +0200

    slider widget styling

--- app/design/frontend/Elogic/base/web/js/mywidget.js
+++ app/design/frontend/Elogic/base/web/js/mywidget.js
@@ -2,27 +2,27 @@ define([
     'jquery',
     'jquery-ui-modules/widget',
     'slickSlider'
-    ], function ($) {
+], function ($) {
     'use strict';
 
-        $.widget('elogic.carousel', {
-            options:{
-                slickOptions: {
-                    infinite: true,
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                    prevArrow: <span> prev</span>,
-                    nextArrow: <span> next</span>,
-                    responsive: [
-                    ]
-                }
-            },
-            _create: function () {
-                if (this.element.hasClass('slick-initialized')) {
-                    this.element.slick('unslick');
-                }
-                this.element.slick(this.options.slickOptions);
+    $.widget('elogic.carousel', {
+        options: {
+            slickOptions: {
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: true,
+                prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">prev</button>',
+                nextArrow: '<button class="slick-next" aria-label="Next" type="button">Next</button>',
+                responsive: []
             }
-        });
+        },
+        _create: function () {
+            if (this.element.hasClass('slick-initialized')) {
+                this.element.slick('unslick');
+            }
+            this.element.slick(this.options.slickOptions);
+        }
+    });
     return $.elogic.carousel;
 });
